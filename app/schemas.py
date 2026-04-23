a58221ac509aa938b4e033eb9e747cefcc10d843
rudraksh-codes <rudraksh.malhotra.098@gmail.com>
2026-04-23 18:38:02 +0530

    added nginx

--- app/schemas.py
+++ app/schemas.py
@@ -1,20 +1,36 @@
-from pydantic import BaseModel, EmailStr , conint
+from pydantic import BaseModel, EmailStr, conint
 from typing import Optional
 from datetime import datetime
 
-class PostBase(BaseModel) : 
-    title : str 
-    content : str 
-    published : bool = True 
 
-class PostResponse(PostBase):
-    id : int
-    created_at : datetime
-    owner_id : int 
-    owner : UserOut
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
 
     class Config:
         orm_mode = True
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class PostResponse(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+    owner: UserOut
+
+    class Config:
+        orm_mode = True
+
 
 class PostOut(BaseModel):
     Post: PostResponse
@@ -24,40 +40,25 @@ class PostOut(BaseModel):
         orm_mode = True
 
 
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
 
 
-class UserOut(BaseModel) :
-    id : int
-    email : EmailStr
-    created_at : datetime
-    
-    class Config : 
-        orm_mode = True 
- 
-
-class PostCreate(PostBase) : 
-    pass 
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
 
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
-class UserCreate(BaseModel) : 
-    email : EmailStr
-    password : str 
+class TokenData(BaseModel):
+    id: Optional[int] = None
 
- 
-class UserLogin(BaseModel) : 
-    email : EmailStr
-    password : str 
 
-class Token(BaseModel) : 
-    access_token : str 
-    token_type : str 
-
-class TokenData(BaseModel) : 
-    id : Optional[int] = None 
-
-class Vote(BaseModel) : 
-    post_id : str
-    dir : conint(le= 1)
-
+class Vote(BaseModel):
+    post_id: str
+    dir: conint(le=1)
